bcc0e29fc5fdeae3a6da961d2beb75b954819f23
yousra shleibik <143129587+YousraShleibik@users.noreply.github.com>
2025-09-09 18:11:07 -0600

    Add day_one_example hello world project

---
+++ day_one_example/src/tests.rs
@@ -0,0 +1,23 @@
+pub fn compute (a: u32, b: u32) -> u32 {
+ a + b * 2
+}
+
+
+
+
+#[cfg(test)]
+mod tests {
+use create::compute;
+
+#[test]
+fn  case(){
+assert_eq!(compute(1,2),5);
+
+}
+#[test]
+fn  case_two(){
+assert_eq!(compute(0,0),0);
+
+
+}
+}
