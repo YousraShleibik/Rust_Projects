b3aa552253e1e2d6bf6d531a91ccf0e61ce0186d
yousra shleibik <143129587+YousraShleibik@users.noreply.github.com>
2025-09-09 20:18:09 -0600

    Remove compute function and related tests; add quadratic function usage in main

--- day_one_example/src/main.rs
+++ day_one_example/src/main.rs
@@ -1,26 +1,31 @@
 
-fn compute(a: u32, b: u32) -> u32 {
-    a + b * 2
-}
 
-#[cfg(test)]
-mod tests {
-    use crate::compute;
-
-    #[test]
-    fn case() {
-        assert_eq!(compute(1, 2), 5);
-    }
-
-    #[test]
-    fn case_two() {
-        assert_eq!(compute(0, 0), 0);
-    }
-}
+// fn compute(a: u32, b: u32) -> u32 {
+//     a + b * 2
+// }
 
 
+// #[cfg(test)]
+// mod tests {
+//     use crate::compute;
+//
+//     #[test]
+//     fn case() {
+//         assert_eq!(compute(1, 2), 5);
+//     }
+//
+//     #[test]
+//     fn case_two() {
+//         assert_eq!(compute(0, 0), 0);
+//     }
+// }
+
+
+use day_one_example::{quadratic};
 
 fn main() {
     println!("Hello, world!");
+
+    println!("quadratic(1, 2, 3, 4) = {}", quadratic(1, 2, 3, 4));
 
 }
