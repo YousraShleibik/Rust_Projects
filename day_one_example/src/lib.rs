a60374e4207a5c9d161f345b4cb87d0a179224a3
yousra shleibik <143129587+YousraShleibik@users.noreply.github.com>
2025-09-09 20:55:08 -0600

    Add dot_product function and corresponding test case in lib.rs

--- day_one_example/src/lib.rs
+++ day_one_example/src/lib.rs
@@ -18,6 +18,20 @@ pub fn scale_vector(point: f32, vector:(f32,f32))-> (f32,f32){
     (point*vector.0, point*vector.1)
 }
 
+
+//Problem Three
+//function: dot_product
+//Description: takes two 2-tuples which represent two dimensional vectors and calculates the dot product of the vectors. which
+// is calculated as (x1 * x2) + (y1 * y2) where (x1, y1) and (x2, y2) are the two input vectors.
+//input ((f32, f32), (f32, f32))
+//output f32
+
+pub fn dot_product(vector1:(f32,f32), vector2:(f32,f32))-> f32{
+    vector1.0 * vector2.0 + vector1.1 * vector2.1
+}
+
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -29,5 +43,9 @@ mod tests {
     #[test]
     fn test_scale_vector() {
         assert_eq!(scale_vector(5.0, (3.0, 4.0)), (15.0, 20.0));
+    }
+    #[test]
+    fn test_dot_product(){
+        assert_eq!(dot_product((2.0, 5.0), (7.0, 1.0)), 19.0); // 2*7 + 5*1
     }
 }
