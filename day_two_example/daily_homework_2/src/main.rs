24e8224d4a273d394485e098a70022a82ead4b6a
yousra shleibik <143129587+YousraShleibik@users.noreply.github.com>
2025-09-20 23:28:25 -0600

    Added all_different function

--- day_two_example/daily_homework_2/src/main.rs
+++ day_two_example/daily_homework_2/src/main.rs
@@ -9,15 +9,32 @@ fn multiply_array(arr: &[u32]) -> u32 {
     }
     result
 }
-//all_different
 
+//all_different
+fn all_different(arr: &[u32]) -> bool {
+    for i in 0..arr.len() {
+        for j in (i + 1)..arr.len() {
+            if arr[i] == arr[j] {
+                println!("Duplicate found: {}", arr[i]);
+                return false; // found a duplicate
+                
+            }
+        }
+    }
+    true // no duplicates found
+    
+}
 
 fn main() {
      
     let arr1 = [2, 3, 4];
-    //let arr2 = [5, 6, 7];
+    let arr2 = [6, 6, 6];
 
     // Multiply all numbers in arr1 → 2*3*4 = 24 
     println!("multiply_array(arr1) = {}", multiply_array(&arr1));
+
+    // Check if all numbers in arr1 are different → true
+     println!("all_different(arr1) = {}", all_different(&arr2));
+
     
 }
